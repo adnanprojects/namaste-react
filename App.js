c2c12efbd3aaea8c8630364a6f1769cc52fb515c
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-28 20:21:35 +0530

    Add file

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const element = React.createElement('h1', {}, 'Hi There from React!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
