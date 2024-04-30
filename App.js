1165c2f9c0be33e8ea81c584ac7c5630b859a760
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-30 16:23:50 +0530

    Deleted Files

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const element = React.createElement('h1', {}, 'Hi There from React!');
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(element);
--- dist/index.6bd02f5a.js
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-const element = React.createElement("h1", {}, "Hi There from React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
