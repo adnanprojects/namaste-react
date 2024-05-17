5f1bf6fc562eb077068ec296c979dc3a2a0096a3
adnanprojects <adnanchaudhary99@gmail.com>
2024-05-17 17:59:28 +0530

    Add Parcel & React

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, 'This is heading inside div')));
-
+const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, 'This is heading inside div using react')));
 
 root.render(parent);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "This is heading inside div")));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
