cce7138dd11823b6d9a64a0a941f5d4bf8be92e8
adnanprojects <adnanchaudhary99@gmail.com>
2024-05-19 15:31:12 +0530

    Update .gitignore

--- dist/index.6bd02f5a.js
+++
@@ -1,9 +0,0 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("div", {
-    id: "parent"
-}, React.createElement("div", {
-    id: "child"
-}, React.createElement("h1", {}, "This is heading inside div")));
-root.render(parent);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
