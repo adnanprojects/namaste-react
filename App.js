6c6a84ce5cbb1bc64b190b6f8f25e9eb355e8758
adnanprojects <adnanchaudhary99@gmail.com>
2024-05-24 14:58:17 +0530

    Add Components

--- App.js
+++ App.js
@@ -1,8 +1,50 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
+/** Blueprint
+ * Header
+ *  - logo
+ *  - Nav
+ * Body
+ *  - search
+ *  - card container
+ *      - cards
+ * Footer
+ *  - copyright
+ *  - links
+ *  - address
+ */
+
+const Header = () => {
+    return (
+        <div className="header">
+            Header
+        </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            Body
+        </div>
+    );
+}
+
+const Footer = () => {
+    return (
+        <div className="footer">Footer</div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="app">
+            App
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, 'This is heading inside div using react')));
-
-root.render(parent);
+root.render(<App />);
