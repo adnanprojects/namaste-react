5ce09c4aa379b32442221608c1bee31532f139ba
adnanprojects <adnanchaudhary99@gmail.com>
2024-05-30 00:35:42 +0530

    Add Live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockData";
+import { useState, useEffect } from "react";
+import { SWIGGY_API } from "../utils/constant";
 
 const Body = () => {
+
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(SWIGGY_API);
+        const json = await data.json();
+        // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
     return (
         <div className="body">
             <div className="search">Search</div>
+            <div className="filter" onClick={() => {
+                const filteredList = listOfRestaurant.filter(res => res.info.avgRating >= 4)
+                setListOfRestaurant(filteredList)
+            }} >Top Rated Restaurant</div>
             <div className="res-container">
-                {resList.map(res => <RestaurantCard resData={res} key={res.info.id} />)}
+                {listOfRestaurant.map(res => <RestaurantCard resData={res} key={res.info.id} />)}
             </div>
         </div>
     );
