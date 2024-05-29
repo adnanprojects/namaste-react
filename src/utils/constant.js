728ca4fd37c6f3b5944d45daec8af06c8911abeb
adnanprojects <adnanchaudhary99@gmail.com>
2024-05-30 00:33:51 +0530

    Add Swiggy API

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,6 @@
 export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 
 export const LOGO_URL = "https://images-platform.99static.com/v84irzbNBd5aawXGKXfH4SEjcn0=/0x0:960x960/500x500/top/smart/99designs-contests-attachments/117/117132/attachment_117132760";
+
+// Swiggy live API
+export const SWIGGY_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.61450&lng=77.30630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
