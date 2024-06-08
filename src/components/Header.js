12626a012031f73bad75cc0fa554349c603b423d
adnanprojects <adnanchaudhary99@gmail.com>
2024-06-08 22:04:10 +0530

    Add links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
@@ -12,9 +13,15 @@ const Header = () => {
             </div>
             <div className="nav-item">
                 <ul>
-                    <li>Home</li>
-                    <li>Contact Us</li>
-                    <li>About Us</li>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact Us</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
                     <li>Cart</li>
                     <button onClick={() => {
                         log === 'Login' ? setLog('Logout') : setLog('Login');
