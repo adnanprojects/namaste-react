6bb899fb5adc2bf4389a38519049c2a9b7417b46
adnanprojects <adnanchaudhary99@gmail.com>
2024-06-12 12:56:04 +0530

    Add Menu Comp

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,23 @@
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+
+const RestaurantMenu = () => {
+    const { resId } = useParams();
+    const resInfo = useRestaurantMenu(resId);
+
+    if (resInfo === null) return <Shimmer />
+
+    const { name, costForTwoMessage, cuisines } = resInfo?.cards[2]?.card?.card?.info;
+    const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+    return (
+        <div className="menu">
+            <h1>{name}</h1>
+            <em>{cuisines.join(', ')} - {costForTwoMessage}</em>
+            <ul>{itemCards.map(item => <li key={item.card.info.id}>{item.card.info.name} - Rs. {(item.card.info.defaultPrice || item.card.info.finalPrice || item.card.info.price) / 100} </li>)}</ul>
+        </div>
+    );
+}
+
+export default RestaurantMenu;
