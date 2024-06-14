e8b433499866d3f9b5ca797af6077bb07d0c963f
adnanprojects <adnanchaudhary99@gmail.com>
2024-06-14 21:38:26 +0530

    Style with TailwindCSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,35 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
 
     const [log, setLog] = useState('Login');
+    const onlineStatus = useOnlineStatus();
 
     return (
-        <div className="header">
-            <div className="logo-container">
-                <Link to='/'><img className="logo" src={LOGO_URL} /></Link>
+        <div className="flex items-center justify-between shadow-lg mb-5">
+            <div className=''>
+                <Link to='/'><img className="w-[120px]" src={LOGO_URL} /></Link>
             </div>
-            <div className="nav-item">
-                <ul>
-                    <li>
+            <div className=''>
+                <ul className="flex">
+                    <li className="border p-2">
+                        Online Status : {onlineStatus ? '🟢' : '🔴'}
+                    </li>
+                    <li className="p-2">
                         <Link to='/'>Home</Link>
                     </li>
-                    <li>
+                    <li className="p-2">
                         <Link to='/contact'>Contact Us</Link>
                     </li>
-                    <li>
+                    <li className="p-2">
                         <Link to='/about'>About</Link>
                     </li>
-                    <li>Cart</li>
+                    <li className="p-2">
+                        <Link to='/cart'>Cart</Link>
+                    </li>
                     <button onClick={() => {
                         log === 'Login' ? setLog('Logout') : setLog('Login');
                     }} >{log}</button>
